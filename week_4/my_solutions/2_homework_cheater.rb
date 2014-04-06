0fc717d0cf272c3910e701a41538e7aed3c8c381
kevinkang88 <kevinkang88@gmail.com>
2014-04-05 20:27:32 -0700

    add reflection hw cheater

--- week_4/my_solutions/2_homework_cheater.rb
+++ week_4/my_solutions/2_homework_cheater.rb
@@ -66,13 +66,7 @@ def essay_writer(title, topic, date, thesis_statment, gender = "it")
     else               pronoun = gender 
   end 
   
-  puts title 
-  puts
-  print topic_final , " played an important role in making changes to the society around #{date}. "
-  print "I wanted to learn more about #{pronoun}. "
-  print "This essay is going to outline the history of #{last_name || topic}. "
-  print "#{thesis_statment} "
-  puts "Now you know why #{last_name || topic} was a seminal piece in our history."
+  return "#{title}\n\n#{topic_final} played an important role in making changes to the\nsociety around #{date}. I wanted to learn more about #{pronoun}.\nThis essay is going to outline the history of #{last_name || topic}.\n#{thesis_statment} Now you know why #{last_name || topic} was a \nseminal piece in our history.\n"
 end
 
 
@@ -81,32 +75,18 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-puts essay_writer("Title", "Kevin Kang", 2014, "thesis.", 'male') 
-
-# pass in a name and see if it divides perfectly
-# pass in a place for topic and see if it comes out right in topic final
-#pass in male for gender and see if prooun equals him
-#pass in female for gender and see if pronoun equals her
-#pass in non for gender and see if pronoun equals it
-
+puts essay_writer("Title", "Kevin Kang", 2014, "thesis.", 'male') == "Title\n\nKang Kevin played an important role in making changes to the\nsociety around 2014. I wanted to learn more about him.\nThis essay is going to outline the history of Kang.\nthesis. Now you know why Kang was a \nseminal piece in our history.\n"
+puts essay_writer("Title", "Kevin Kang", 2014, "thesis.") == "Title\n\nKang Kevin played an important role in making changes to the\nsociety around 2014. I wanted to learn more about it.\nThis essay is going to outline the history of Kang.\nthesis. Now you know why Kang was a \nseminal piece in our history.\n"
+puts essay_writer("Title", "DBC", 2014, "thesis.") == "Title\n\nDBC played an important role in making changes to the\nsociety around 2014. I wanted to learn more about it.\nThis essay is going to outline the history of DBC.\nthesis. Now you know why DBC was a \nseminal piece in our history.\n"
 
 
 
 # 5. Reflection 
 
 
-When I was brainstorming I was thinking about using loop to generate randomized sentences
-but realized using interpolation was an easier approach to this problem. During the process
-I discovered a new way to use or (||) instead of just placing True/False values on either
-side, placing variables. If the variable on the left is truthy it will return the variable
-on the left side. however if the variable on the left is false-y then it returns the variable
-on the right. 
-What parts of your strategy worked? What problems did you face?
-What questions did you have while coding? What resources did you find to help you answer them?
-What concepts are you having trouble with, or did you just figure something out? If so, what?
-Did you learn any new skills or tricks?
-How confident are you with each of the learning objectives?
-Which parts of the challenge did you enjoy?
-Which parts of the challenge did you find tedious?
-
-
+# When I was brainstorming I was thinking about using loop to generate randomized sentences
+# but realized using interpolation was an easier approach to this problem. During the process
+# I discovered a new way to use or (||) instead of just placing expressions returning true or
+# false on either side. If the variable on the left is truthy it will return the variable
+# on the left side. however if the variable on the left is false-y then it returns the variable
+# on the right. 
