24d01bad25b302f1e192cda0a4ec7cc3ef725536
kevinkang88 <kevinkang88@gmail.com>
2014-04-16 20:29:09 -0700

    add playlist challenge solution

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,22 +1,110 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Adam Dziuk].
 
 
 # Pseudocode
-
+#No actual pseudo coding was involved in this challenge. Just looking at the error-msg and 
+#adding appropriate methods and its components to pass tests
 
 
 # Initial Solution
 
+# class Song
+#   attr_reader :song, :artist 
 
+#   def initialize(song,artist) 
+#     @song = song
+#     @artist = artist
+#   end
+  
+#   def play
+#     puts "Now playing #{song} by #{artist}"
+#   end
+# end
 
+# class Playlist
+  
+  
+#   def initialize(*songs)
+#     @playlist = []
+#     songs.each do |song|
+#       @playlist << song
+#     end 
+#   end
+  
+#   def add(*songs)
+#     songs.each do |song|
+#       @playlist << song
+#     end
+#   end
+  
+#   def remove(*songs)
+#     songs.each do |song|
+#       @playlist.delete(song)
+#     end
+#   end
+  
+#   def num_of_tracks
+#     @playlist.size
+#   end
+  
+#   def includes?(song)
+#     @playlist.include?(song)
+#   end
+  
+#   def play_all
+#     @playlist.each do |song|
+#       song.play
+#     end
+#   end
+# end
 
 # Refactored Solution
 
 
+class Song
+  attr_reader :song, :artist 
 
+  def initialize(song,artist) 
+    @song = song
+    @artist = artist
+  end
+  
+  def play
+    puts "Now playing #{song} by #{artist}"
+  end
+end
+
+class Playlist
+  
+  
+  def initialize(*songs)
+    @playlist = []
+    songs.each {|song| @playlist << song}
+  end
+  
+  def add(*songs)
+    songs.each {|song| @playlist << song}
+  end
+  
+  def remove(*songs)
+    songs.each {|song| @playlist.delete(song)}
+  end
+  
+  def num_of_tracks
+    @playlist.size
+  end
+  
+  def includes?(song)
+    @playlist.include?(song)
+  end
+  
+  def play_all
+    @playlist.each {|song| song.play}
+  end
+end
 
 
 
@@ -39,7 +127,9 @@ my_playlist.play_all
 my_playlist.display
 
 
-
-
-
 # Reflection 
+
+# We worked through the exercise based on the error messages shown in terminal 
+# when we ran the file. The messages were formatted such way that it was easily 
+# readable. It was good practice using splat operator which I learned few exercises 
+# ago. Also became more familiar with reading error messages.
